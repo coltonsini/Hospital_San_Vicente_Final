fce4a6133c55882c3067d2b35794f5cf7a7868ef
Juan José Ordoñez Villalobos <xxjuanjose15cxx@gmail.com>
2021-11-11 02:08:48 -0500

    changes
    sss

--- src/routes/citas.js
+++ src/routes/citas.js
@@ -4,11 +4,14 @@ const router = express.Router();
 const Cita = require("../models/Citas");
 const { isAuthenticated } = require('../helpers/auth');
 
-router.get('/citas/crear', isAuthenticated, (req, res) => {
+// router.get('/citas/crear', isAuthenticated, (req, res) => {
+
+router.get('/citas/crear', (req, res) => {
     res.render('citas/nueva-cita');
 });
+// router.post('/citas/nueva-cita', isAuthenticated, async (req, res) =>{
 
-router.post('/citas/nueva-cita', isAuthenticated, async (req, res) =>{
+router.post('/citas/nueva-cita', async (req, res) =>{
     const { title, description } = req.body;
     const errors = [];
     if(!title){
@@ -34,23 +37,29 @@ router.post('/citas/nueva-cita', isAuthenticated, async (req, res) =>{
 });
 
 router.get('/citas', isAuthenticated, async (req, res) => {
-    const notes = await Cita.find({user: req.user.id}).sort({date: 'desc'});
+    const Cita = await Cita.find({user: req.user.id}).sort({date: 'desc'});
     res.render('/citas/todas-citas', { citas });
 });
 
 router.get('/citas/edit/:id', async (req, res) => {
     const cita = await Cita.findById(req.params.id);
+    if(cota.user != req.user.id){
+        req.flash('error_msg', 'Not Authorized');
+        return res.redirect('/citas');
+    }
     res.render('citas/edit-note', {cita});
 });
 
 router.put('/citas/edit-cita/:id', async (req, res) => {
     const {title, description} = req.body;
     await Cita.findByIdAndUpdate(req.params.id, { title, description});
+    req.flash('sucess_msg', 'Cita Actualizada Satisfactoriamente');
     res.redirect('/citas');
 })
 
-router.delete('/citas/eliminar/:id', (req, res) => {
-    Cita.findByIdAndDelete(req.params.id);
+router.delete('/citas/eliminar/:id', async (req, res) => {
+    await Cita.findByIdAndDelete(req.params.id);
+    req.flash('sucess_msg', 'Cita Eliminada Satisfactioriamente');
     res.redirect('/citas');
 });
 
