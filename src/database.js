36f884aba023323e4d10fbd2c1d2520b7da4ccfa
Juan José Ordoñez Villalobos <xxjuanjose15cxx@gmail.com>
2021-11-14 18:29:17 -0500

    database change
    changes

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const db_ = process.env.DB_url
 
 console.log('base de datos : ', db_);
-mongoose.connect(db_, {
+mongoose.connect(db_, { useNewUrlParser: true, useUnifiedTopology: true
 });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error: '));
