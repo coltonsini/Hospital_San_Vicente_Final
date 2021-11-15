0c199ea745c0a24bacebb980183c780710aca687
Juan José Ordoñez Villalobos <xxjuanjose15cxx@gmail.com>
2021-11-15 11:25:43 -0500

    databasechange
    changes to database

--- src/database.js
+++ src/database.js
@@ -2,14 +2,11 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const db_ = process.env.DB_url
 
-console.log('base de datos : ', db_);
-module.exports = async () => {
-    await mongoose.connect(db_, { 
+mongoose.connect(db_, { 
         keepAlive: true,
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        useFindAndModify: false,
-})};
+});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error: '));
@@ -17,3 +14,4 @@ db.on('error', console.error.bind(console, 'connection error: '));
 db.once('open', () => {
     console.log('db logs are connected 🥵🥵🥵🥵🥵🥵');
 });
+
